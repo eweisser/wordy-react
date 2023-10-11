ba98d396e1e36373efa59fd6b002fc1a3f017e02
Eric Weisser <ericwweisser@gmail.com>
2023-10-10 23:34:53 -0500

    first user input

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import { LEXICON } from './components/lexicon.js';
+import StartMenu from './components/start-menu.js';
+import AIMenu from './components/AI-menu.js';
+import Game from './components/game.js';
 import './App.css';
+import { useState } from 'react';
+import AIMenuButton from './components/AI-menu-button.js';
 
 function App() {
+
+  const [startMenuAppear, setStartMenuAppear] = useState(true);
+  const [aiMenuAppear, setAiMenuAppear] = useState(false);
+  const [gameAppear, setGameAppear] = useState(false);
+
+  const [activeAi, setActiveAi] = useState(null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="LeftFrame">
+        {startMenuAppear && <StartMenu showAiMenu={() => {setStartMenuAppear(false); setAiMenuAppear(true)} } />}
+
+        {/* {aiMenuAppear && <AIMenu sendAi={(chosenAi) => {setAiMenuAppear(false); setGameAppear(true); alert(chosenAi)}} />} */}
+        {aiMenuAppear && <AIMenu>
+          <AIMenuButton AiName="Andrew" pickAi={() => {setActiveAi("Andrew"); setAiMenuAppear(false); setGameAppear(true)}}></AIMenuButton>
+          <AIMenuButton AiName="Bianca" pickAi={() => {setActiveAi("Bianca"); setAiMenuAppear(false); setGameAppear(true)}}></AIMenuButton>
+          <AIMenuButton AiName="Charlie" pickAi={() => {setActiveAi("Charlie"); setAiMenuAppear(false); setGameAppear(true)}}></AIMenuButton>
+        </AIMenu>}
+
+        {gameAppear && <Game sendActiveAi={activeAi} />}
+      </div>
+      <div className="RightFrame">
+      </div>
+      
     </div>
   );
 }
--- src/components/game.js
+++ src/components/game.js
@@ -1,9 +1,26 @@
 import '../App.css';
+import { useState } from 'react';
+import { useCallback } from 'react';
 
 const Game = ({sendActiveAi}) => {
 
     // const dayOrNight = data.weather[0].icon[2] + "Time";
     // document.body.setAttribute('class', dayOrNight);
+
+    const [whoGoesFirst, setWhoGoesFirst] = useState("...");
+
+    const handleKeyDown = (event) => {
+        setWhoGoesFirst(event.key);
+        // if ("1234567890.+-*/()^".includes(event.key)) {
+        //   sendExpressionToDisplay(event.key);
+        // }
+      };
+
+    const whoGoesFirstInput = useCallback((inputElement) => {
+        if (inputElement) {
+            inputElement.focus();
+         }
+    }, []);
 
     return (
         <div id="game">
@@ -14,8 +31,8 @@ const Game = ({sendActiveAi}) => {
                 </div>
             </div>
             <div className="playerInputContainer">
-                <div autoFocus className="playerInput">
-                    ...
+                <div tabIndex="0" onKeyDown={handleKeyDown} className="playerInput" ref={whoGoesFirstInput}>
+                    {whoGoesFirst}
                 </div>
                 <div className="playerInputSpace"></div>
             </div>
