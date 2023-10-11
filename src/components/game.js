a3d468d2a87141678ed34058346caababf59f67f
Eric Weisser <ericwweisser@gmail.com>
2023-10-11 12:43:42 -0500

    started generate divs by map function

--- src/components/game.js
+++ src/components/game.js
@@ -1,16 +1,24 @@
 import '../App.css';
 import { useState } from 'react';
 import { useCallback } from 'react';
+import { createElement } from 'react';
 
 const Game = ({sendActiveAi}) => {
 
     // const dayOrNight = data.weather[0].icon[2] + "Time";
     // document.body.setAttribute('class', dayOrNight);
 
-    const [whoGoesFirst, setWhoGoesFirst] = useState("...");
+    const dialogueBoxArray = ["first","second","third","fourth","fifth"];
+
+    const [currentUserInput, setCurrentUserInput] = useState("...");
+    const [whoGoesFirst, setWhoGoesFirst] = useState(null);
 
     const handleKeyDown = (event) => {
-        setWhoGoesFirst(event.key);
+        if ("12".includes(event.key)) {
+            setCurrentUserInput(event.key);
+        } else if (event.key === "Enter") {
+            dialogueBoxArray.push("new");
+        }
         // if ("1234567890.+-*/()^".includes(event.key)) {
         //   sendExpressionToDisplay(event.key);
         // }
@@ -24,7 +32,12 @@ const Game = ({sendActiveAi}) => {
 
     return (
         <div id="game">
-            <div className="compDialogueContainer">
+            
+            {dialogueBoxArray.map(builder => (
+                <div className="compDialogueContainer">Hello!</div>
+            ))}
+
+            {/* <div className="compDialogueContainer">
                 <div className="compDialogueSpace"></div>
                 <div className="compDialogue">
                     Hi, I'm {sendActiveAi}. Enter 1 if you want to go first, or 2 for me to go first.
@@ -32,10 +45,10 @@ const Game = ({sendActiveAi}) => {
             </div>
             <div className="playerInputContainer">
                 <div tabIndex="0" onKeyDown={handleKeyDown} className="playerInput" ref={whoGoesFirstInput}>
-                    {whoGoesFirst}
+                    {currentUserInput}
                 </div>
                 <div className="playerInputSpace"></div>
-            </div>
+            </div> */}
         </div>
     );
 }
