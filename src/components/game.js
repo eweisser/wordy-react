a2c88239892ab1514d72bc93319472c1802dd4a2
Eric Weisser <ericwweisser@gmail.com>
2023-10-13 15:38:23 -0500

    successfully displays new element on key press

--- src/components/game.js
+++ src/components/game.js
@@ -8,16 +8,21 @@ const Game = ({sendActiveAi}) => {
     // const dayOrNight = data.weather[0].icon[2] + "Time";
     // document.body.setAttribute('class', dayOrNight);
 
-    const dialogueBoxArray = ["first","second","third","fourth","fifth"];
+    const dialogueBoxArray = ["Hi, I'm "+sendActiveAi+". Enter 1 if you want to go first, or 2 for me to go first.","..."];
 
     const [currentUserInput, setCurrentUserInput] = useState("...");
     const [whoGoesFirst, setWhoGoesFirst] = useState(null);
+    const [allDialogues, setAllDialogues] = useState(["Hi, I'm "+sendActiveAi+". Enter 1 if you want to go first, or 2 for me to go first.","..."]);
 
     const handleKeyDown = (event) => {
         if ("12".includes(event.key)) {
             setCurrentUserInput(event.key);
+            // console.log(event.key);
         } else if (event.key === "Enter") {
-            dialogueBoxArray.push("new");
+            // dialogueBoxArray.push("new");
+            // console.log(dialogueBoxArray);
+            setAllDialogues(allDialogues.concat(["new"]) );
+            // console.log(allDialogues);
         }
         // if ("1234567890.+-*/()^".includes(event.key)) {
         //   sendExpressionToDisplay(event.key);
@@ -30,12 +35,29 @@ const Game = ({sendActiveAi}) => {
          }
     }, []);
 
+    const makeDialogueBox = (item) => {
+        const itemNumber = dialogueBoxArray.indexOf(item);
+        if (itemNumber % 2 === 0) {
+            // console.log(item);
+            return <div key={itemNumber} className="compDialogueContainer">
+                        <div className="compDialogueSpace"></div>
+                        <div className="compDialogue">{item}</div>
+                    </div>
+        } else {
+            // console.log(item);
+            return <div key={itemNumber} className="playerInputContainer">
+                        <div tabIndex="0" onKeyDown={handleKeyDown} className="playerInput" ref={whoGoesFirstInput}>{item}</div>
+                        <div className="playerInputSpace"></div>
+                    </div>
+        }
+    }
+
+    var mappedDialogueBoxes = allDialogues.map(makeDialogueBox);
+
     return (
         <div id="game">
             
-            {dialogueBoxArray.map(builder => (
-                <div className="compDialogueContainer">Hello!</div>
-            ))}
+            {mappedDialogueBoxes}
 
             {/* <div className="compDialogueContainer">
                 <div className="compDialogueSpace"></div>
