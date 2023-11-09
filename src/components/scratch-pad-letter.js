7fc736eb10b8d3c692a40c004775b7f5e4c0e35e
Eric Weisser <ericwweisser@gmail.com>
2023-11-09 14:40:05 -0600

    implemented letter scratchpad cycles three states

--- src/components/scratch-pad-letter.js
+++ src/components/scratch-pad-letter.js
@@ -1,10 +1,26 @@
 import '../App.css';
+import { useState } from 'react';
 
-const ScratchPadLetter = ({Letter, cycleLetterButton}) => {
+const ScratchPadLetter = ({Letter}) => {
+
+    const [letterKnowledge, setLetterKnowledge] = useState("letter_neutral");
+
+    const cycleLetterButton = () => {
+        if (letterKnowledge == "letter_neutral") {
+            setLetterKnowledge("letter_eliminated");
+            // alert("Now eliminated!");
+        } else if (letterKnowledge == "letter_eliminated") {
+            setLetterKnowledge("letter_proven");
+            // alert("Now proven!");
+        } else if (letterKnowledge == "letter_proven") {
+            setLetterKnowledge("letter_neutral");
+            // alert("Now neutral!");
+        }
+    }
 
     return (
         // <div className="ai-menu-button" onPress={() => pickAi({AiName})}>{AiName}</div>
-        <div className="scratch-pad-letter-button" onClick={() => cycleLetterButton()}>{Letter}</div>
+        <div className={letterKnowledge} onClick={() => cycleLetterButton()}>{Letter}</div>
     );
 }
 
